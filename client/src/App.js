b66a4e45727b7a32280de0e3e8553ed1b0dc2c8a
Judeva <joanna.doudeva@gmail.com>
2021-06-02 18:53:57 +0300

    Hero section

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
 import '@fontsource/roboto';
+import { Fragment } from 'react';
 import './App.css';
 import Header from './components/header/Header';
+import Hero from './components/hero-section/Hero';
 
 function App() {
   return (
+    <Fragment>
     <Header/>
+    <Hero/>
+    </Fragment>
   );
 }
 
---
+++ client/src/components/hero-section/Hero.js
@@ -0,0 +1,15 @@
+import './Hero.css'
+
+const Hero = () => {
+    return (
+        <div className="hero-section">
+            <h2>Movies</h2>
+            <p id='hero-section-p'>Search your favorite movies and rate for the best. Create your personal collection of top nominated.</p>
+            <input
+                id="hero-section-btn"
+                type='submit'
+                value='Search' />
+        </div>
+    );
+}
+export default Hero;
